f9d8f49d17e6cdaa7580e5dda805b13723f6c8f7
Erwin Mueller <erwin.mueller@deventm.org>
2012-08-01 21:42:30 +0200

    Implement start class

--- groovybash-cliapp/src/main/java/com/anrisoftware/groovybash/application/Application.java
+++ groovybash-cliapp/src/main/java/com/anrisoftware/groovybash/application/Application.java
@@ -1,5 +1,36 @@
 package com.anrisoftware.groovybash.application;
 
+import javax.inject.Inject;
+
+import com.anrisoftware.groovybash.parameter.ParameterParser;
+import com.google.inject.Injector;
+
+/**
+ * Parse the command line arguments and run the script.
+ * 
+ * @author Erwin Mueller, erwin.mueller@deventm.org
+ * @since 1.0
+ */
 public class Application {
 
+	private final ParameterParser parser;
+
+	@Inject
+	Application(ParameterParser parser) {
+		this.parser = parser;
+	}
+
+	/**
+	 * Parse the command line arguments and run the script.
+	 * 
+	 * @param args
+	 *            the command line arguments.
+	 * 
+	 * @param injector
+	 *            the {@link Injector}.
+	 */
+	public void start(String[] args, Injector injector) {
+		parser.parseParameter(args, injector);
+		parser.getBashParser().run();
+	}
 }
---
+++ groovybash-cliapp/src/main/java/com/anrisoftware/groovybash/application/ApplicationModule.java
@@ -0,0 +1,26 @@
+package com.anrisoftware.groovybash.application;
+
+import com.anrisoftware.groovybash.core.buildins.BuildinsModule;
+import com.anrisoftware.groovybash.core.environment.EnvironmentModule;
+import com.anrisoftware.groovybash.core.parser.ParserModule;
+import com.anrisoftware.groovybash.core.plugins.PluginsModule;
+import com.anrisoftware.groovybash.parameter.ParameterModule;
+import com.google.inject.AbstractModule;
+
+/**
+ * Install all needed modules for the application.
+ * 
+ * @author Erwin Müller, erwin.mueller@deventm.org
+ * @since 1.0
+ */
+class ApplicationModule extends AbstractModule {
+
+	@Override
+	protected void configure() {
+		install(new ParameterModule());
+		install(new ParserModule());
+		install(new EnvironmentModule());
+		install(new PluginsModule());
+		install(new BuildinsModule());
+	}
+}
