96f49e806d619f09ff4f63001898a4d827b34626
Erwin Mueller <erwin.mueller@deventm.org>
2012-08-11 15:36:17 +0200

    Install the build-in modules
    #1502

--- groovybash-core/src/main/java/com/anrisoftware/groovybash/core/buildins/BuildinsModule.java
+++ groovybash-core/src/main/java/com/anrisoftware/groovybash/core/buildins/BuildinsModule.java
@@ -18,20 +18,48 @@
  */
 package com.anrisoftware.groovybash.core.buildins;
 
+import com.anrisoftware.groovybash.core.buildins.cdbuildin.CdModule;
+import com.anrisoftware.groovybash.core.buildins.echobuildin.EchoModule;
+import com.anrisoftware.groovybash.core.buildins.listfilesbuildin.ListFilesModule;
+import com.anrisoftware.groovybash.core.buildins.logbuildins.DebugModule;
+import com.anrisoftware.groovybash.core.buildins.logbuildins.ErrorModule;
+import com.anrisoftware.groovybash.core.buildins.logbuildins.InfoModule;
+import com.anrisoftware.groovybash.core.buildins.logbuildins.TraceModule;
+import com.anrisoftware.groovybash.core.buildins.logbuildins.WarnModule;
+import com.anrisoftware.groovybash.core.buildins.parsebuildin.ParseModule;
+import com.anrisoftware.groovybash.core.buildins.pwdbuildin.PwdModule;
+import com.anrisoftware.groovybash.core.buildins.returns.ReturnsModule;
+import com.anrisoftware.groovybash.core.buildins.runbuildin.RunModule;
 import com.google.inject.AbstractModule;
 import com.google.inject.Provides;
 
 /**
- * Provides a default of the the standard input and output streams for build-ins
- * commands.
+ * Install the build-in modules. Provides a default of the the standard input
+ * and output streams for build-ins commands.
  * 
  * @author Erwin Mueller, erwin.mueller@deventm.org
- * @since 1.0
+ * @since 0.1
  */
 public class BuildinsModule extends AbstractModule {
 
 	@Override
 	protected void configure() {
+		installBuildins();
+	}
+
+	private void installBuildins() {
+		install(new CdModule());
+		install(new EchoModule());
+		install(new ListFilesModule());
+		install(new DebugModule());
+		install(new ErrorModule());
+		install(new InfoModule());
+		install(new TraceModule());
+		install(new WarnModule());
+		install(new ParseModule());
+		install(new PwdModule());
+		install(new ReturnsModule());
+		install(new RunModule());
 	}
 
 	/**
