4c3e08e751d74a0f3f3508d092ebd9aa837f9ed8
Erwin Mueller <erwin.mueller@deventm.org>
2012-08-12 11:48:51 +0200

    Add documentation and gpl license for the cd build-in
    Finish #1459

--- groovybash-buildins/src/main/java/com/anrisoftware/groovybash/buildins/cdbuildin/CdModule.java
+++ groovybash-buildins/src/main/java/com/anrisoftware/groovybash/buildins/cdbuildin/CdModule.java
@@ -1,7 +1,24 @@
+/*
+ * Copyright 2012 Erwin Müller <erwin.mueller@deventm.org>
+ * 
+ * This file is part of groovybash-buildins.
+ * 
+ * groovybash-buildins is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option) any
+ * later version.
+ * 
+ * groovybash-buildins is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * groovybash-buildins. If not, see <http://www.gnu.org/licenses/>.
+ */
 package com.anrisoftware.groovybash.buildins.cdbuildin;
 
 import com.anrisoftware.groovybash.buildins.returns.ReturnsModule;
-import com.anrisoftware.groovybash.buildins.utils.ReadUntilCharacterModule;
 import com.anrisoftware.groovybash.core.Buildin;
 import com.google.inject.AbstractModule;
 
@@ -9,14 +26,13 @@ import com.google.inject.AbstractModule;
  * Binds the cd build-in command.
  * 
  * @author Erwin Mueller, erwin.mueller@deventm.org
- * @since 1.0
+ * @since 0.1
  */
 public class CdModule extends AbstractModule {
 
 	@Override
 	protected void configure() {
 		bind(Buildin.class).to(CdBuildin.class);
-		install(new ReadUntilCharacterModule());
 		install(new ReturnsModule());
 	}
 }
