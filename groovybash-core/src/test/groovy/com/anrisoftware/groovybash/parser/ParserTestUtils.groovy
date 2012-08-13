cb333680f41cc8003f1f7b5fcb81cd0687b68cf1
Erwin Mueller <erwin.mueller@deventm.org>
2012-08-13 12:03:35 +0200

    Test imports in the script parser
    #1502

--- groovybash-core/src/test/groovy/com/anrisoftware/groovybash/parser/ParserTestUtils.groovy
+++ groovybash-core/src/test/groovy/com/anrisoftware/groovybash/parser/ParserTestUtils.groovy
@@ -2,13 +2,20 @@ package com.anrisoftware.groovybash.parser
 
 import org.junit.Before
 
+import com.anrisoftware.globalpom.utils.TestUtils
 import com.anrisoftware.groovybash.core.Environment
 import com.anrisoftware.groovybash.core.factories.BashParserFactory
 import com.google.inject.AbstractModule
 import com.google.inject.Guice
 import com.google.inject.Injector
 
-class ParserTestUtils {
+/**
+ * Utilities to test the script parser.
+ * 
+ * @author Erwin Mueller, erwin.mueller@deventm.org
+ * @since 0.2
+ */
+class ParserTestUtils extends TestUtils {
 
 	Injector injector
 
@@ -16,6 +23,16 @@ class ParserTestUtils {
 
 	def environmentStub
 
+	def byteOutputStream
+
+	def outputStream
+
+	/**
+	 * Binds {@link Environment} to the environment stub.
+	 * 
+	 * @author Erwin Mueller, erwin.mueller@deventm.org
+	 * @since 0.2
+	 */
 	static class EnvironmentStubModule extends AbstractModule {
 
 		def environmentStub
@@ -30,10 +47,21 @@ class ParserTestUtils {
 	void beforeTest() {
 		injector = createInjector()
 		factory = injector.getInstance BashParserFactory
+		createOutputStream()
+	}
+
+	private createOutputStream() {
+		byteOutputStream = new ByteArrayOutputStream(1024)
+		outputStream = new PrintStream(byteOutputStream)
+		System.setOut outputStream
 	}
 
 	Injector createInjector() {
 		environmentStub = new StubEnvironment()
 		Guice.createInjector new ParserModule(), new EnvironmentStubModule(environmentStub: environmentStub)
+	}
+
+	String getOutput() {
+		byteOutputStream.toString()
 	}
 }
--- groovybash-core/src/test/groovy/com/anrisoftware/groovybash/parser/StubEnvironment.groovy
+++ groovybash-core/src/test/groovy/com/anrisoftware/groovybash/parser/StubEnvironment.groovy
@@ -7,65 +7,56 @@ import org.slf4j.Logger
 import com.anrisoftware.groovybash.core.Environment
 import com.google.inject.Injector
 
+/**
+ * Stub for the {@link Environment}.
+ * 
+ * @author Erwin Mueller, erwin.mueller@deventm.org
+ * @since 0.2
+ */
 class StubEnvironment implements Environment {
 
 	@Override
 	void setInjector(Injector injector) {
-		// TODO Auto-generated method stub
-
 	}
 
 	@Override
 	void setArguments(String[] args) {
-		// TODO Auto-generated method stub
-
 	}
 
 	@Override
 	String[] getArguments() {
-		// TODO Auto-generated method stub
 		return null
 	}
 
 	@Override
 	void setWorkingDirectory(File directory) {
-		// TODO Auto-generated method stub
-
 	}
 
 	@Override
 	File getWorkingDirectory() {
-		// TODO Auto-generated method stub
 		return null
 	}
 
 	@Override
 	void setScriptLoggerContext(Class<?> context) {
-		// TODO Auto-generated method stub
-
 	}
 
 	@Override
 	Logger getScriptLogger() {
-		// TODO Auto-generated method stub
 		return null
 	}
 
 	@Override
 	File getUserHome() {
-		// TODO Auto-generated method stub
 		return null
 	}
 
 	@Override
 	Future<?> submitTask(Runnable task) {
-		// TODO Auto-generated method stub
 		return null
 	}
 
 	@Override
 	void close() {
-		// TODO Auto-generated method stub
-
 	}
 }
