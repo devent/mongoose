10900119eba1cc3a87b40b0f80a817d61682f538
Erwin Mueller <erwin.mueller@deventm.org>
2013-04-14 21:35:27 +0200

    Implement pwd build-in command

---
+++ mongoose-buildins/src/main/java/com/anrisoftware/mongoose/buildins/pwdbuildin/PwdBuildin.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2012 Erwin Müller <erwin.mueller@deventm.org>
+ * 
+ * This file is part of groovybash-core.
+ * 
+ * groovybash-core is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option) any
+ * later version.
+ * 
+ * groovybash-core is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * groovybash-core. If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.anrisoftware.mongoose.buildins.pwdbuildin;
+
+import java.io.PrintStream;
+
+import com.anrisoftware.mongoos.command.AbstractCommand;
+import com.anrisoftware.mongoose.api.exceptions.ExecutionException;
+
+/**
+ * The build-in command {@code pwd}. Prints the absolute path of the current
+ * working directory to the standard output.
+ * 
+ * @author Erwin Mueller, erwin.mueller@deventm.org
+ * @since 1.0
+ */
+class PwdBuildin extends AbstractCommand {
+
+	/**
+	 * Returns the current working directory.
+	 */
+	@Override
+	public PwdBuildin call() throws ExecutionException {
+		PrintStream stream = new PrintStream(getOutput());
+		stream.println(getEnvironment().getWorkingDirectory());
+		stream.flush();
+		return this;
+	}
+
+	/**
+	 * Returns the name {@code pwd}.
+	 */
+	@Override
+	public String getName() {
+		return PwdService.ID;
+	}
+}
---
+++ mongoose-buildins/src/main/java/com/anrisoftware/mongoose/buildins/pwdbuildin/PwdModule.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2012 Erwin Müller <erwin.mueller@deventm.org>
+ * 
+ * This file is part of groovybash-core.
+ * 
+ * groovybash-core is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option) any
+ * later version.
+ * 
+ * groovybash-core is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * groovybash-core. If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.anrisoftware.mongoose.buildins.pwdbuildin;
+
+import com.anrisoftware.mongoose.api.commans.Command;
+import com.anrisoftware.mongoose.api.commans.CommandFactory;
+import com.google.inject.AbstractModule;
+import com.google.inject.assistedinject.FactoryModuleBuilder;
+
+/**
+ * Installs the {@code pwd} build-in command factory.
+ * 
+ * @author Erwin Mueller, erwin.mueller@deventm.org
+ * @since 1.0
+ */
+public class PwdModule extends AbstractModule {
+
+	@Override
+	protected void configure() {
+		install(new FactoryModuleBuilder().implement(Command.class,
+				PwdBuildin.class).build(CommandFactory.class));
+	}
+}
