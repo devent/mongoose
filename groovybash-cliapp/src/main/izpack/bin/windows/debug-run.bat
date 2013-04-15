8f5edecf1952fe46dba9b118aff6a055a7b26d2a
Erwin Mueller <erwin.mueller@deventm.org>
2013-04-15 22:22:00 +0200

    Remove old groovybash

--- groovybash-cliapp/src/main/izpack/bin/windows/debug-run.bat
+++
@@ -1,4 +0,0 @@
-set mainjar=%CD%\lib\${project.artifactId}-${project.version}-jar-with-dependencies.jar
-set log="-Dlog4j.configuration=file:///%CD%/etc/debug-log4j.properties"
-
-java %log% -jar %mainjar%
