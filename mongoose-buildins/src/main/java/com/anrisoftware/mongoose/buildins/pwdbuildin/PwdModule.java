fc401d2e7e7a1dd3107f1c4306c6eb7c400793ea
Erwin Mueller <erwin.mueller@deventm.org>
2013-04-17 01:10:27 +0200

    Leave modules private

--- mongoose-buildins/src/main/java/com/anrisoftware/mongoose/buildins/pwdbuildin/PwdModule.java
+++ mongoose-buildins/src/main/java/com/anrisoftware/mongoose/buildins/pwdbuildin/PwdModule.java
@@ -29,7 +29,7 @@ import com.google.inject.assistedinject.FactoryModuleBuilder;
  * @author Erwin Mueller, erwin.mueller@deventm.org
  * @since 1.0
  */
-public class PwdModule extends AbstractModule {
+class PwdModule extends AbstractModule {
 
 	@Override
 	protected void configure() {
