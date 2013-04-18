598fe41c4abc331d195b92b658bc6200772781cd
Erwin Mueller <erwin.mueller@deventm.org>
2013-04-18 14:21:29 +0200

    Add old build-in commands tests

---
+++ mongoose-buildins/src/test/groovy/com/anrisoftware/groovybash/buildins/listfilesbuildin/ListFilesTest.groovy
@@ -0,0 +1,135 @@
+/*
+ * Copyright 2012 Erwin Müller <erwin.mueller@deventm.org>
+ * 
+ * This file is part of groovybash-buildins.
+ * 
+ * groovybash-buildins is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option) any
+ * later version.
+ * 
+ * groovybash-buildins is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * groovybash-buildins. If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.anrisoftware.groovybash.buildins.listfilesbuildin
+
+import org.junit.Before
+import org.junit.BeforeClass
+import org.junit.Test
+
+import com.anrisoftware.globalpom.utils.TestUtils
+import com.anrisoftware.groovybash.buildins.BuildinTestUtils
+import com.anrisoftware.mongoose.buildins.listfilesbuildin.ListFilesBuildin;
+import com.anrisoftware.mongoose.buildins.listfilesbuildin.ListFilesModule;
+import com.google.common.io.Files
+
+/**
+ * Test the build-in command {@code listFiles}.
+ * 
+ * @author Erwin Mueller, erwin.mueller@deventm.org
+ * @since 0.1
+ */
+class ListFilesTest extends BuildinTestUtils {
+
+	static File directory
+
+	@BeforeClass
+	static void beforeClass() {
+		directory = new TestUtils().createTempDirectory { d ->
+			Files.touch new File(d, "foo.txt")
+			Files.touch new File(d, "bar.txt")
+			Files.touch new File(d, "baz.txt")
+			Files.touch new File(d, "foo.jpg")
+			Files.touch new File(d, "bar.jpg")
+			Files.touch new File(d, "baz.jpg")
+			Files.touch new File(d, "foo.x")
+			Files.touch new File(d, "bar.x")
+			Files.touch new File(d, "baz.x")
+		}
+	}
+
+	static file(def name) {
+		new File(directory, name)
+	}
+
+	@Before
+	void beforeTest() {
+		super.beforeTest()
+		injector = injector.createChildInjector new ListFilesModule()
+	}
+
+	@Test
+	void "list file name"() {
+		environment.workingDirectory = directory
+		def result = createBuildin(ListFilesBuildin, ["foo.txt"])()
+		assert result.files == [file("foo.txt")]
+		assertStringContent result.toString(), "[$directory/foo.txt]"
+	}
+
+	@Test
+	void "listFiles buildin with file names from list"() {
+		environment.workingDirectory = directory
+		def list = ["foo.txt", "bar.txt"]
+		def result = createBuildin(ListFilesBuildin, [list])()
+		assert result.files == [file("foo.txt"), file("bar.txt")]
+		assertStringContent result.toString(), "[$directory/foo.txt, $directory/bar.txt]"
+	}
+	
+	@Test
+	void "listFiles buildin iterate over files"() {
+		environment.workingDirectory = directory
+		def list = ["foo.txt", "bar.txt"]
+		def result = createBuildin(ListFilesBuildin, [list])()
+		def files = []
+		result.each {
+			files << it
+		}
+		assert files == [file("foo.txt"), file("bar.txt")]
+	}
+	
+	@Test
+	void "listFiles buildin with no arguments"() {
+		environment.workingDirectory = directory
+		def result = createBuildin(ListFilesBuildin)()
+		assert result.files == [file("baz.x"), file("bar.x"), file("foo.x"), file("baz.jpg"), file("bar.jpg"), file("foo.jpg"), file("baz.txt"), file("bar.txt"), file("foo.txt")]
+		assertStringContent result.toString(), "[$directory/baz.x, $directory/bar.x, $directory/foo.x, $directory/baz.jpg, $directory/bar.jpg, $directory/foo.jpg, $directory/baz.txt, $directory/bar.txt, $directory/foo.txt]"
+	}
+
+	@Test
+	void "listFiles buildin with wildcat"() {
+		environment.workingDirectory = directory
+		def result = createBuildin(ListFilesBuildin, ["*.txt"])()
+		assert result.files == [file("baz.txt"), file("bar.txt"), file("foo.txt")]
+		assertStringContent result.toString(), "[$directory/baz.txt, $directory/bar.txt, $directory/foo.txt]"
+	}
+
+	@Test
+	void "listFiles buildin with wildcat and test against a list"() {
+		environment.workingDirectory = directory
+		def result = createBuildin(ListFilesBuildin, ["*.txt"])()
+		assert result == [file("baz.txt"), file("bar.txt"), file("foo.txt")]
+	}
+
+	@Test
+	void "listFiles buildin with multiple wildcats"() {
+		environment.workingDirectory = directory
+		def result = createBuildin(ListFilesBuildin, ["*.jpg", "*.txt"])()
+		assert result.files == [file("baz.jpg"), file("bar.jpg"), file("foo.jpg"), file("baz.txt"), file("bar.txt"), file("foo.txt")]
+		assertStringContent result.toString(), "[$directory/baz.jpg, $directory/bar.jpg, $directory/foo.jpg, $directory/baz.txt, $directory/bar.txt, $directory/foo.txt]"
+	}
+
+	@Test
+	void "listFiles buildin with multiple wildcats from list"() {
+		environment.workingDirectory = directory
+		def list = ["*.jpg", "*.txt"]
+		def result = createBuildin(ListFilesBuildin, [list])()
+		assert result.files == [file("baz.jpg"), file("bar.jpg"), file("foo.jpg"), file("baz.txt"), file("bar.txt"), file("foo.txt")]
+		assertStringContent result.toString(), "[$directory/baz.jpg, $directory/bar.jpg, $directory/foo.jpg, $directory/baz.txt, $directory/bar.txt, $directory/foo.txt]"
+	}
+
+}
---
+++ mongoose-buildins/src/test/groovy/com/anrisoftware/groovybash/buildins/runbuildin/OutputTaskTest.groovy
@@ -0,0 +1,114 @@
+package com.anrisoftware.groovybash.buildins.runbuildin
+
+import groovy.util.logging.Slf4j
+
+import org.junit.Test
+import org.perfidix.Benchmark
+import org.perfidix.annotation.Bench
+import org.perfidix.ouput.TabularSummaryOutput
+
+import com.anrisoftware.mongoose.buildins.runbuildin.OutputTask;
+
+/**
+ * Benchmark the output of a process to the standard output stream.
+ * 
+ * @author Erwin Mueller, erwin.mueller@deventm.org
+ * @since 0.1
+ */
+@Slf4j
+class OutputTaskTest {
+
+	/**
+	 * Outputs a given string in the standard output of the process.
+	 * 
+	 * @author Erwin Mueller, erwin.mueller@deventm.org
+	 * @since 0.1
+	 */
+	static class DummyProcess extends Process {
+
+		String outputString
+
+		DummyProcess(String string) {
+			outputString = string
+		}
+
+		@Override
+		OutputStream getOutputStream() {
+		}
+
+		@Override
+		InputStream getInputStream() {
+			new ByteArrayInputStream(outputString.getBytes("UTF-8"))
+		}
+
+		@Override
+		InputStream getErrorStream() {
+		}
+
+		@Override
+		int waitFor() throws InterruptedException {
+		}
+
+		@Override
+		int exitValue() {
+		}
+
+		@Override
+		void destroy() {
+		}
+	}
+
+	def output
+
+	def outputStream
+
+	def bigtext = new BigText().run()
+
+	def smalltext = "small text"
+
+	Process process
+
+	@Test
+	void "run benchmarks"() {
+		def output = new ByteArrayOutputStream()
+		def benchmark = new Benchmark()
+		benchmark.add(this)
+		def result = benchmark.run()
+		new TabularSummaryOutput(new PrintStream(output)).visitBenchmark(result)
+		log.info "\n{}", output.toString()
+	}
+
+	@Test
+	void "read from input and output"() {
+		output = new ByteArrayOutputStream()
+		outputStream = new PrintStream(output)
+		process = new DummyProcess(smalltext)
+		def task = new OutputTask(process.inputStream, outputStream)
+		task.run()
+		assert output.toString() == smalltext
+	}
+
+	@Bench(runs = 100, beforeFirstRun = "setup big text")
+	void "read from input and output with big text"() {
+		def task = new OutputTask(process.inputStream, outputStream)
+		task.run()
+	}
+
+	void "setup big text"() {
+		output = new ByteArrayOutputStream()
+		outputStream = new PrintStream(output)
+		process = new DummyProcess(bigtext)
+	}
+
+	@Bench(runs = 100, beforeFirstRun = "setup small text")
+	void "read from input and output with small text"() {
+		def task = new OutputTask(process.inputStream, outputStream)
+		task.run()
+	}
+
+	void "setup small text"() {
+		output = new ByteArrayOutputStream()
+		outputStream = new PrintStream(output)
+		process = new DummyProcess(smalltext)
+	}
+}
