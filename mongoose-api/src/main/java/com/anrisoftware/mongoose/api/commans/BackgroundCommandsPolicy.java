f8a364739ee342761ea8aee2e8244c1a5fed05c7
Erwin Mueller <erwin.mueller@deventm.org>
2013-05-03 12:52:10 +0200

    Add formatter for background command policy

--- mongoose-api/src/main/java/com/anrisoftware/mongoose/api/commans/BackgroundCommandsPolicy.java
+++ mongoose-api/src/main/java/com/anrisoftware/mongoose/api/commans/BackgroundCommandsPolicy.java
@@ -1,5 +1,9 @@
 package com.anrisoftware.mongoose.api.commans;
 
+import java.text.FieldPosition;
+import java.text.Format;
+import java.text.ParsePosition;
+
 /**
  * Policy how to proceed with commands started in the background.
  * 
@@ -21,5 +25,47 @@ public enum BackgroundCommandsPolicy {
 	/**
 	 * Cancel all background commands.
 	 */
-	CANCEL
+	CANCEL;
+
+	/**
+	 * Formatter for {@link BackgroundCommandsPolicy}.
+	 */
+	@SuppressWarnings("serial")
+	public static Format POLICY_FORMAT = new Format() {
+
+		@Override
+		public Object parseObject(String source, ParsePosition pos) {
+			return parse(source, pos);
+		}
+
+		/**
+		 * @see #parseObject(String, ParsePosition)
+		 */
+		public BackgroundCommandsPolicy parse(String source, ParsePosition pos) {
+			try {
+				source = source.substring(pos.getIndex());
+				BackgroundCommandsPolicy item = valueOf(source.toUpperCase());
+				pos.setErrorIndex(-1);
+				pos.setIndex(source.length());
+				return item;
+			} catch (IllegalArgumentException e) {
+				pos.setIndex(0);
+				pos.setErrorIndex(0);
+				return null;
+			}
+		}
+
+		@Override
+		public StringBuffer format(Object obj, StringBuffer buff,
+				FieldPosition pos) {
+			if (obj instanceof BackgroundCommandsPolicy) {
+				format(buff, (BackgroundCommandsPolicy) obj);
+			}
+			return buff;
+		}
+
+		private void format(StringBuffer buff, BackgroundCommandsPolicy obj) {
+			buff.append(obj.toString());
+		}
+	};
 }
