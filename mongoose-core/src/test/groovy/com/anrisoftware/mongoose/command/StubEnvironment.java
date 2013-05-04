bc39620ae2c5246984b712bdeb4432790c2d2d0d
Erwin Mueller <erwin.mueller@deventm.org>
2013-05-04 18:06:58 +0200

    Add execution model to stub

--- mongoose-core/src/test/groovy/com/anrisoftware/mongoose/command/StubEnvironment.java
+++ mongoose-core/src/test/groovy/com/anrisoftware/mongoose/command/StubEnvironment.java
@@ -14,6 +14,7 @@ import org.slf4j.Logger;
 import com.anrisoftware.mongoose.api.commans.Command;
 import com.anrisoftware.mongoose.api.environment.BackgroundCommandsPolicy;
 import com.anrisoftware.mongoose.api.environment.Environment;
+import com.anrisoftware.mongoose.api.environment.ExecutionMode;
 import com.anrisoftware.mongoose.api.exceptions.CommandException;
 
 /**
@@ -149,6 +150,18 @@ class StubEnvironment implements Environment {
 	@Override
 	public List<Future<?>> shutdown() throws InterruptedException {
 		executor.shutdown();
+		return null;
+	}
+
+	@Override
+	public void setExecutionMode(ExecutionMode mode) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public ExecutionMode getExecutionMode() {
+		// TODO Auto-generated method stub
 		return null;
 	}
 }
