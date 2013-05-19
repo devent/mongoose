53c8c61f30588c8e6c938505687d16d1794f971f
Erwin Mueller <erwin.mueller@deventm.org>
2013-05-19 20:19:17 +0200

    Add create command with the dependencies
    #2391

--- mongoose-core/src/main/java/com/anrisoftware/mongoose/command/CommandLoader.java
+++ mongoose-core/src/main/java/com/anrisoftware/mongoose/command/CommandLoader.java
@@ -1,11 +1,17 @@
 package com.anrisoftware.mongoose.command;
 
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Map;
 import java.util.ServiceLoader;
 
+import javax.inject.Inject;
 import javax.inject.Singleton;
 
 import com.anrisoftware.mongoose.api.commans.Command;
 import com.anrisoftware.mongoose.api.commans.CommandService;
+import com.anrisoftware.mongoose.api.environment.Environment;
+import com.anrisoftware.mongoose.api.exceptions.CommandException;
 
 /**
  * Loads a command as a Java service.
@@ -15,6 +21,13 @@ import com.anrisoftware.mongoose.api.commans.CommandService;
  */
 @Singleton
 public class CommandLoader {
+
+	private final CommandLoaderLogger log;
+
+	@Inject
+	public CommandLoader(CommandLoaderLogger logger) {
+		this.log = logger;
+	}
 
 	/**
 	 * Loads the command with the specified name.
@@ -37,6 +50,67 @@ public class CommandLoader {
 			}
 		}
 		return null;
+	}
+
+	/**
+	 * Loads the commands and setups the output targets and input source.
+	 * 
+	 * @param command
+	 *            the command {@link String} name.
+	 * 
+	 * @param environment
+	 *            the {@link Environment} of the command.
+	 * 
+	 * @param named
+	 *            the named parameter {@link Map} of the command.
+	 * 
+	 * @param outputTarget
+	 *            the standard output target {@link OutputStream} stream, can be
+	 *            {@code null}.
+	 * 
+	 * @param errorTarget
+	 *            the standard error target {@link OutputStream} stream, can be
+	 *            {@code null}.
+	 * 
+	 * @param inputSource
+	 *            the standard input source {@link InputStream} stream, can be
+	 *            {@code null}.
+	 * 
+	 * @param args
+	 *            the arguments for the command.
+	 * 
+	 * @return the loaded {@link Command}.
+	 * 
+	 * @throws CommandException
+	 *             if there was an error loading the command.
+	 */
+	public Command createCommand(String command, Environment environment,
+			Map<String, Object> named, OutputStream outputTarget,
+			OutputStream errorTarget, InputStream inputSource, Object... args)
+			throws CommandException {
+		try {
+			Command cmd = loadCommand(command);
+			log.checkCommand(cmd, command);
+			cmd.setEnvironment(environment);
+			setupStreams(cmd, outputTarget, errorTarget, inputSource);
+			cmd.args(named, args);
+			return cmd;
+		} catch (Exception e) {
+			throw log.errorLoadCommand(e, command);
+		}
+	}
+
+	private void setupStreams(Command mount, OutputStream outputTarget,
+			OutputStream errorTarget, InputStream inputSource) throws Exception {
+		if (outputTarget != null) {
+			mount.setOutput(outputTarget);
+		}
+		if (errorTarget != null) {
+			mount.setError(errorTarget);
+		}
+		if (inputSource != null) {
+			mount.setInput(inputSource);
+		}
 	}
 
 }
---
+++ mongoose-core/src/main/java/com/anrisoftware/mongoose/command/CommandLoaderLogger.java
@@ -0,0 +1,41 @@
+package com.anrisoftware.mongoose.command;
+
+import static org.apache.commons.lang3.Validate.notNull;
+
+import javax.inject.Singleton;
+
+import com.anrisoftware.globalpom.log.AbstractLogger;
+import com.anrisoftware.mongoose.api.commans.Command;
+import com.anrisoftware.mongoose.api.exceptions.CommandException;
+
+/**
+ * Logging messages for {@link CommandLoader}.
+ * 
+ * @author Erwin Mueller, erwin.mueller@deventm.org
+ * @since 1.0
+ */
+@Singleton
+class CommandLoaderLogger extends AbstractLogger {
+
+	private static final String COMMAND_NULL = "Could not find command '%s',";
+	private static final String ERROR_LOAD_COMMAND = "Error load command";
+	private static final String ERROR_LOAD_COMMAND_MESSAGE = "Error load command '%s'.";
+
+	/**
+	 * Create logger for {@link CommandLoader}.
+	 */
+	public CommandLoaderLogger() {
+		super(CommandLoader.class);
+	}
+
+	CommandException errorLoadCommand(Exception e, String name) {
+		return logException(
+				new CommandException(ERROR_LOAD_COMMAND, e).addContext("name",
+						name), ERROR_LOAD_COMMAND_MESSAGE, name);
+	}
+
+	void checkCommand(Command cmd, String command) {
+		notNull(cmd, COMMAND_NULL, command);
+	}
+
+}
